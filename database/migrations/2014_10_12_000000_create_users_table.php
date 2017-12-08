7129447ffba1a00919baebc4fc820b5c5eac6529
yuzesun <yuzesun@unomaha.edu>
2017-12-08 11:34:48 -0600

    delete foreign key relationship

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,9 +25,9 @@ class CreateUsersTable extends Migration
             $table->timestamps();
         });
 
-        Schema::table('users', function(Blueprint $table) {
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-        });
+//        Schema::table('users', function(Blueprint $table) {
+//            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+//        });
     }
 
     /**
