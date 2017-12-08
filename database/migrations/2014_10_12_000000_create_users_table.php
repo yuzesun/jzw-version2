6d8639273df59623ad29703289d1aec6fa872fe8
yuzesun <yuzesun@unomaha.edu>
2017-12-08 11:32:52 -0600

    Add foreign key

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,14 +20,14 @@ class CreateUsersTable extends Migration
             $table->string('phone_number');
             $table->string('email')->unique();
             $table->string('password');
-//            $table->integer('role_id')->unsigned();
+            $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
 
-//        Schema::table('users', function(Blueprint $table) {
-//            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-//        });
+        Schema::table('users', function(Blueprint $table) {
+            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+        });
     }
 
     /**
