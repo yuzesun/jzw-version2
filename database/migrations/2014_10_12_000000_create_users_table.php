2f68761f49a7e255542b14cd2035a9373d4e1fbe
yuzesun <yuzesun@unomaha.edu>
2017-12-08 11:25:08 -0600

    Testing

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,20 +14,21 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->string('first_name');
             $table->string('last_name');
             $table->string('phone_number');
             $table->string('email')->unique();
             $table->string('password');
-            $table->integer('role_id')->unsigned();
+//            $table->integer('role_id')->unsigned();
             $table->rememberToken();
             $table->timestamps();
         });
 
-        Schema::table('users', function(Blueprint $table) {
-            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-        });
+//        Schema::table('users', function(Blueprint $table) {
+//            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
+//        });
     }
 
     /**
