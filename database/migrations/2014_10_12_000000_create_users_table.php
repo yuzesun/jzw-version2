e9df0d5a75310a34e6f1fd01ba095183b32f46f5
yuzesun <yuzesun@unomaha.edu>
2017-12-08 11:38:28 -0600

    add foreign key setup

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -24,10 +24,6 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
         });
-
-//        Schema::table('users', function(Blueprint $table) {
-//            $table->foreign('role_id')->references('id')->on('roles')->onDelete('cascade');
-//        });
     }
 
     /**
